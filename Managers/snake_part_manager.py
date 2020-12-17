a587c64085fe62a7f5facb9d58dfb92f942751f8
Jelena Beader <jelenabeader.sm@gmail.com>
2020-12-17 02:57:16 +0100

    test commit..

--- Managers/snake_part_manager.py
+++ Managers/snake_part_manager.py
@@ -9,5 +9,5 @@ class SnakePartManager:
 
     def increase_snake(self, active_snake, snake_tail_previous_x, snake_tail_previous_y):
         #TODO: Add snake tail creation here when pictures are added
-        new_part = SnakePart(snake_tail_previous_x, snake_tail_previous_y , self.part_width, self.part_height, SnakePartType.BODY)
+        new_part = SnakePart(snake_tail_previous_x, snake_tail_previous_y, self.part_width, self.part_height, SnakePartType.BODY)
         active_snake.add_snake_part(new_part)
