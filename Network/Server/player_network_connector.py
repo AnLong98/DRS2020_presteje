6f05ccd02bd640bc3a53642a78c3b594d05453c7
Predrag <svetoizvorac@yahoo.com>
2021-01-13 19:53:47 +0100

    Added network connector logic and safeguard against client disconnecting in pre game phase

---
+++ Network/Server/player_network_connector.py
@@ -0,0 +1,113 @@
+import select
+import socket
+
+from Network.socket_manager import SocketManager, NetworkPackageFlag
+
+class ConnectorHandle:
+    def __init__(self, client_socket, socket_manager, address):
+        self.client_socket = client_socket
+        self.address = address
+        self.socket_manager = socket_manager
+        self.ready = False
+        self.username = ""
+
+    def set_ready(self):
+        self.ready = True
+
+    def set_username(self, name):
+        self.username = name
+
+class PlayerNetworkConnector:
+    HOST = socket.gethostbyname(socket.gethostname())  # Current PC's IP address
+    PORT = 50005  # Arbitrary non-privileged port
+    MAX_CONNECTIONS_LISTEN = 30 #Maximal number of connections for listen socket
+
+    def await_client_connections(self, clients_number):
+        socket_handle_dict = {}
+        clients_dict = {}
+        try:
+            listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            listen_socket.bind((self.HOST, self.PORT))
+            listen_socket.listen(self.MAX_CONNECTIONS_LISTEN)
+            print("Server is listening on IP and Port ", self.HOST, self.PORT)
+        except Exception:
+            print("Cloud not listen at server default port. Game aborted!")
+            return None
+
+        connection_attempts = 0
+        connected_clients = 0
+        clients_ready = 0
+
+        readable_sockets = []
+        readable_sockets.append(listen_socket)
+        while connection_attempts < self.MAX_CONNECTIONS_LISTEN:
+            read, write, error = select.select(readable_sockets, [], [], 2)
+            if not read:
+                continue
+
+            for socketc in read:
+                # new client wants to connect, and number of connected clients is less than requested
+                if socketc == listen_socket and connected_clients < clients_number:
+                    connector_handle = self.__accept_connection(listen_socket)
+                    socket_handle_dict[connector_handle.client_socket] = connector_handle
+                    connector_handle.client_socket.setblocking(False)
+                    readable_sockets.append(connector_handle.client_socket)
+                    connected_clients += 1
+                    connection_attempts += 1
+
+                elif socketc != listen_socket: # connected client sent us his username or has disconnected
+                    try:
+                        if self.__handle_game_request(socketc, socket_handle_dict, clients_dict):
+                            clients_ready += 1
+                    except Exception:
+                        print("Error receiving data from connecting client")
+                        if socket_handle_dict[socketc].ready:
+                            # If client was ready but disconnected decrease number of ready clients
+                            clients_dict.pop(socket_handle_dict[socketc].username)
+                            clients_ready -= 1
+                        self.__handle_broken_socket(socketc, socket_handle_dict, readable_sockets)
+                        print("Ready " + str(clients_ready))
+                        connected_clients -= 1
+
+            if clients_ready == clients_number:
+                return clients_dict
+
+
+
+    def __accept_connection(self, listen_socket):
+        client_socket, addr = listen_socket.accept()
+        print("Client with addr " + addr[0] + " has connected")
+
+        return ConnectorHandle(client_socket, SocketManager(client_socket), addr)
+
+    def __handle_game_request(self, client_socket, socket_handle_dict, clients_dict):
+        msg, flag = socket_handle_dict[client_socket].socket_manager.recv_message()
+
+        if flag != NetworkPackageFlag.USERNAME:
+            raise Exception("Protocol error")  # protocol error
+
+        if msg in clients_dict.keys():  # user has chosen an existing user name
+            socket_handle_dict[client_socket].socket_manager.send_message(1, NetworkPackageFlag.USERNAME_INVALID)
+            return False
+        else:
+            socket_handle_dict[client_socket].socket_manager.send_message(1, NetworkPackageFlag.USERNAME_VALID)
+            socket_handle_dict[client_socket].set_ready()
+            socket_handle_dict[client_socket].set_username(msg)
+            clients_dict[msg] = client_socket
+            print("User " + msg + " has joined the game.")
+            return True
+
+
+    def __handle_broken_socket(self, client_socket, handle_dict, readable_sockets):
+        if client_socket in readable_sockets:
+            readable_sockets.remove(client_socket)
+
+        if client_socket in handle_dict.keys():
+            socket_manager = handle_dict[client_socket].socket_manager
+            socket_manager.shutdown()
+            handle_dict.pop(client_socket)
+
+        if not readable_sockets:
+            #This shouldn't ever happen
+            raise Exception("Something is very wrong with listener")
+
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from Managers.movement_manager import MovementManager
 from Models.snake import Snake, SnakeDirection
 from Models.snake_part import SnakePart, SnakePartType
 from Models.user import User
+from Network.Server.player_network_connector import PlayerNetworkConnector
 from Network.Server.server_network_manager import ServerNetworkManager
 from Managers.shift_players_manager import ShiftPlayersManager
 from Managers.snake_part_manager import SnakePartManager
@@ -208,43 +209,45 @@ if __name__ == "__main__":
 
     if clients_number == None and snake_count == None:
         print("Server window has been shutdown!")
-    else:
-        # init game related things hardcoded for prototype
-        print("Server is up and running")
+        sys.exit()
 
-        network_manager = ServerNetworkManager(clients_number)
-        player_names = network_manager.get_client_names
-        table_width = 960
-        table_height = 810
+    # init game related things hardcoded for prototype
+    print("Server is up and running")
 
-        food = []
-        all_snakes = []
+    network_connector = PlayerNetworkConnector()
+    network_manager = ServerNetworkManager(clients_number, network_connector)
+    player_names = network_manager.get_client_names
+    table_width = 960
+    table_height = 810
 
-        players = ServerInitializer().get_players(clients_number, player_names, snake_count)
+    food = []
+    all_snakes = []
 
-        collision_manager = CollisionManager(all_snakes, food, table_width, table_height)
-        food_manager = FoodManager(collision_manager)
-        movement_manager = MovementManager()
-        snake_part_manager = SnakePartManager(part_width, part_height, collision_manager)
-        shift_players_manager = ShiftPlayersManager()
+    players = ServerInitializer().get_players(clients_number, player_names, snake_count)
+
+    collision_manager = CollisionManager(all_snakes, food, table_width, table_height)
+    food_manager = FoodManager(collision_manager)
+    movement_manager = MovementManager()
+    snake_part_manager = SnakePartManager(part_width, part_height, collision_manager)
+    shift_players_manager = ShiftPlayersManager()
 
 
-        # Append all snakes
-        for player in players:
-            all_snakes.extend(player.snakes)
+    # Append all snakes
+    for player in players:
+        all_snakes.extend(player.snakes)
 
-        for i in range(0, 30):
-            food.append(food_manager.generate_food(1, 1, table_width, table_height, 15))
+    for i in range(0, 30):
+        food.append(food_manager.generate_food(1, 1, table_width, table_height, 15))
 
-        food.append(
-            food_manager.generate_food(1, 1, table_width, table_height, 15, True))  # generate superfood
+    food.append(
+        food_manager.generate_food(1, 1, table_width, table_height, 15, True))  # generate superfood
 
-        # Uncomment for testing
-        #for i in range(0, 200):
-            #food.append(food_manager.generate_food(1, 1, all_snakes, food, table_width, table_height, 15,True))  # generate superfood
+    # Uncomment for testing
+    #for i in range(0, 200):
+        #food.append(food_manager.generate_food(1, 1, all_snakes, food, table_width, table_height, 15,True))  # generate superfood
 
-        game = Game(players, food, collision_manager, network_manager, movement_manager, snake_part_manager, food_manager,
-                    shift_players_manager, table_width, table_height)
-        game.run_game()
-        print("Game has started")
+    game = Game(players, food, collision_manager, network_manager, movement_manager, snake_part_manager, food_manager,
+                shift_players_manager, table_width, table_height)
+    game.run_game()
+    print("Game has started")
 
