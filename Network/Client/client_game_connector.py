21be2bedea9cf0f3e8c7cc6ccf2d01213132803e
stefan <stefanbesovic@gmail.com>
2021-01-13 23:21:47 +0100

    Added new window for server parameters(ip and port). Added safe exit for client.

--- Network/Client/client_game_connector.py
+++ Network/Client/client_game_connector.py
@@ -1,32 +1,52 @@
 import socket
+import sys
 
 from GUI.client_start_window import ClientStartWindow
+from GUI.client_server_parameters_window import ClientServerParametersWindow
 from Network.Client.client_network_manager import ClientSocketSender
 from Network.socket_manager import SocketManager, NetworkPackageFlag
 
 
 class ClientGameConnector:
-    # TODO: Let client input these parameters
-    HOST = socket.gethostbyname(socket.gethostname())  # Current PC's IP address
-    PORT = 50005  # The same port as used by the server
+
 
     def connect(self):
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.HOST, self.PORT))
-        socket_sender = ClientSocketSender(client_socket)
-        client_recv = SocketManager(client_socket)
+        server_parameters_window = ClientServerParametersWindow()
+        server_parameters_window.exec()
         while True:
-            client_window = ClientStartWindow()
-            client_window.exec()
+            HOST = server_parameters_window.server_ip
+            PORT = server_parameters_window.server_port
+
+            if HOST is None or PORT is None:
+                return None
+
+            try:
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((HOST, PORT))
+                socket_sender = ClientSocketSender(client_socket)
+                client_recv = SocketManager(client_socket)
+                break
+            except:
+                server_parameters_window.error_label.setText("Invalid IP or PORT, please enter again.")
+                server_parameters_window.exec()
+
+        client_window = ClientStartWindow()
+        client_window.exec()
+
+        while True:
             username = client_window.username
             if username is None:
                 return None
+
             socket_sender.send_game_request(username)
 
             response = client_recv.recv_message()
-            print(response[1])
 
             if response[1] != NetworkPackageFlag.USERNAME_INVALID:
                 client_socket.setblocking(False)
                 return client_socket, socket_sender, username
+            else:
+                client_window.error_label.setText("Username already exists. Try again.")
+                client_window.exec()
+
